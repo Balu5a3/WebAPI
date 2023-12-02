1d2ab9056768d2094d7b839291419fb22008decc
BALA MURALI KRISHNA PENUMARTHI <Balu5a3@users.noreply.github.com>
2023-12-02 17:00:34 -0500

    Initial commit

---
+++ BchatDataRetrieve/CheckUsers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BChatDataAccess;
+
+namespace BchatDataRetrieve
+{
+    public class CheckUsers
+    {
+        public static bool Login(string UserName,string Password)
+        {
+
+            User UserLogin = new User();
+            using (BChatEntities entity = new BChatEntities())
+            {
+                UserLogin = entity.Users.Where(x => x.UserId == UserName && x.FirstName == Password).FirstOrDefault();
+            }
+            return (UserLogin!=null)?true: false;
+        }
+
+    }
+}
---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApi.Controllers
+{
+    
+    public class ValuesController : ApiController
+    {
+        static List<string> strings = new List<string>
+        {
+            "value0","value1","value3"
+        };
+
+        // GET api/values
+        [RequireHttps]
+        public HttpResponseMessage Get()
+        {
+            string userName = Thread.CurrentPrincipal.Identity.Name;
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return strings[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            strings.Add(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+            strings[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            strings.RemoveAt(id); 
+        }
+    }
+}
